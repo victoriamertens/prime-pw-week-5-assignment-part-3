a78cf2cdb16621734ff2e3155ae46c1f48d18339
Victoria Mertens <victoria.mertens44@gmail.com>
2022-10-17 16:14:12 -0500

    Created addToCollection function

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,1 +1,15 @@
-console.log('***** Music Collection *****')
+console.log('***** Music Collection *****');
+let collection = [];
+
+function addToCollection(titleNew, artistNew, yearPublishedNew) {
+  let newCollectionObject = {
+    title: titleNew,
+    artist: artistNew,
+    yearPublished: yearPublishedNew,
+  };
+  collection.push(newCollectionObject);
+  return 'Addition Completed';
+}
+
+console.log(addToCollection('Piano Man', 'Billy Joel', 1973));
+console.log(collection);
