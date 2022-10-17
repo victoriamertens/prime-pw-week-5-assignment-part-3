a462fd10b59d57e17f553cf71c8176a1e4470e07
Victoria Mertens <victoria.mertens44@gmail.com>
2022-10-17 16:39:08 -0500

    Added showCollection function

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -8,8 +8,28 @@ function addToCollection(titleNew, artistNew, yearPublishedNew) {
     yearPublished: yearPublishedNew,
   };
   collection.push(newCollectionObject);
-  return 'Addition Completed';
+  return newCollectionObject;
 }
 
 console.log(addToCollection('Piano Man', 'Billy Joel', 1973));
+console.log(addToCollection('Hunky Dory', 'David Bowie', 1971));
+console.log(addToCollection('Greatest Hits', 'Frank Sinatra', 1950));
+console.log(addToCollection('Lungs', 'Florence and the Mchine', 2010));
+console.log(addToCollection('Dear Agony', 'Breaking Benjamin', 2009));
+console.log(addToCollection('Back from the Dead', 'Halestorm', 2022));
 console.log(collection);
+
+function showCollection(arrParameter) {
+  console.log(arrParameter.length);
+  for (let i = 0; i < arrParameter.length; i++) {
+    console.log(
+      arrParameter[i].title,
+      'by',
+      arrParameter[i].artist,
+      'published in',
+      arrParameter[i].yearPublished
+    );
+  }
+}
+
+console.log(showCollection(collection));
