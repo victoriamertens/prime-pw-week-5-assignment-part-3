e8e2ae7d673ff3b5f4594a366c7555baad27fcd2
Victoria Mertens <victoria.mertens44@gmail.com>
2022-10-18 10:26:22 -0500

    Added gaurd clause to search function

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,22 +1,65 @@
 console.log('***** Music Collection *****');
 let collection = [];
 
-function addToCollection(titleNew, artistNew, yearPublishedNew) {
+function addToCollection(titleNew, artistNew, yearPublishedNew, newTracks) {
   let newCollectionObject = {
     title: titleNew,
     artist: artistNew,
     yearPublished: yearPublishedNew,
+    tracks: newTracks,
   };
   collection.push(newCollectionObject);
   return newCollectionObject;
 }
 
-console.log(addToCollection('Piano Man', 'Billy Joel', 1973));
-console.log(addToCollection('Hunky Dory', 'David Bowie', 1971));
-console.log(addToCollection('Greatest Hits', 'Frank Sinatra', 1950));
-console.log(addToCollection('Lungs', 'Florence and the Mchine', 2010));
-console.log(addToCollection('Dear Agony', 'Breaking Benjamin', 2009));
-console.log(addToCollection('Back from the Dead', 'Halestorm', 2022));
+console.log(
+  addToCollection('Piano Man', 'Billy Joel', 1973, [
+    "Travelin' Prayer",
+    'Piano Man',
+    "Ain't No Crime",
+    "You're My Home",
+  ])
+);
+console.log(
+  addToCollection('Hunky Dory', 'David Bowie', 1971, [
+    'Changes',
+    'Oh! You Pretty Things',
+    'Eight Line Poem',
+    'Life On Mars',
+  ])
+);
+console.log(
+  addToCollection('Greatest Hits', 'Frank Sinatra', 1950, [
+    'Strangers in the Night',
+    'Summer Wind',
+    'It was a Very Good Year',
+    'Somewhere in Your Heart',
+  ])
+);
+console.log(
+  addToCollection('Lungs', 'Florence and the Mchine', 2010, [
+    'Dog Days are over',
+    'RabbitHeart',
+    "Im' not calling you a liar",
+    'Howl',
+  ])
+);
+console.log(
+  addToCollection('Dear Agony', 'Breaking Benjamin', 2009, [
+    'Fade Away',
+    'I will not bow',
+    'Crawl',
+    'Give me a sign',
+  ])
+);
+console.log(
+  addToCollection('Back from the Dead', 'Halestorm', 2022, [
+    'Back from the Dead',
+    'Wicked Ways',
+    'Strange Girl',
+    'Brightside',
+  ])
+);
 console.log(collection);
 
 function showCollection(arrParameter) {
@@ -51,14 +94,23 @@ console.log(findByArtist('Cheap Trick'));
 //Stretch Goals
 function search(inputParameter) {
   let finalArr = [];
+  let errArr = [];
+  for (let i = 0; i < collection.length; i++) {
+    errArr.push(collection[i].title);
+  }
+  if (
+    inputParameter === undefined ||
+    Object.keys(inputParameter).length === 0
+  ) {
+    return errArr;
+  }
+
   for (let i = 0; i < collection.length; i++) {
     if (
       inputParameter.artist === collection[i].artist &&
       inputParameter.year === collection[i].yearPublished
     ) {
       finalArr.push(collection[i]);
-    } else if (inputParameter.artist === '' && inputParameter.year === '') {
-      finalArr.push(collection[i].title);
     }
   }
   return finalArr;
@@ -66,4 +118,5 @@ function search(inputParameter) {
 console.log(collection);
 console.log(search({ artist: 'Halestorm', year: 2022 }));
 console.log(search({ artist: 'Ray Charles', year: 1957 }));
-console.log(search({ artist: '', year: '' }));
+console.log(search());
+console.log(search({}));
